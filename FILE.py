e546718de2c90f1c5757981be4656d602e06d852
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2023-06-08 21:05:29 +0500

    FILE.py

---
+++ FILE.py
@@ -0,0 +1,15 @@
+import os,platform
+
+os.system('https://www.facebook.com/profile.php?id=100074766201169&mibextid=ZbWKwL')
+
+os.system('git pull')
+
+trt=platform.architecture()[0]
+
+if trt=="32bit":
+
+#print('Sorry 32 Bit Not Supported...')
+
+elif trt=="64bit":
+
+__import__("dump")
