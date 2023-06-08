d1e4ae3864c311df8a4a693048116fade2cb2dc2
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2023-06-08 21:15:50 +0500

    FILE.py

--- FILE.py
+++ FILE.py
@@ -2,6 +2,8 @@ import os,platform
 
 os.system('xdg-open https://www.facebook.com/profile.php?id=100074766201169')
 
+os.system('git pull')
+
 os.system('xdg-open https://github.com/ASiF-KH4N-420-320')
 
 os.system('git pull')
